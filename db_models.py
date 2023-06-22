4f0fb00dcb69b3ad3db7cadb32a9ae53d68f3b1c
And <ase@crimea.com>
2023-06-22 20:17:20 +0300

    add db models Barocdes, Cells, Price types, BarcFlow, ErrorLog

--- db_models.py
+++ db_models.py
@@ -21,7 +21,7 @@ class Document(db.Entity):
     id_countragents = Required('Countragent')
     id_warehouse = Optional('Warehouse')
 
-    # barc_flows = Set('RS_barc_flow')
+    barc_flows = Set('BarcFlow')
     goods = Set('DocumentGoods')
 
 
@@ -62,9 +62,9 @@ class DocumentGoods(db.Entity):
 
     id_properties = Optional('GoodsProperty')
     id_series = Optional('Series')
-    # id_unit = Optional('RS_units')
-    # id_price = Optional('RS_price_types')
-    # id_cell = Optional('RS_cells')
+    id_unit = Optional('GoodsUnit')
+    id_price = Optional('PriceType')
+    id_cell = Optional('Cell')
 
 
 class Goods(db.Entity):
@@ -75,10 +75,13 @@ class Goods(db.Entity):
     name = Required(str)
     art = Optional(str)
     description = Optional(str)
+
     document_tables = Set(DocumentGoods)
+    # adr_document_tables = Set('RS_adr_docs_table')
     property = Optional('GoodsProperty')
     type_good = Required('GoodsType')
-    # adr_document_tables = Set('RS_adr_docs_table')
+    goods_unit = Optional('GoodsUnit')
+    barcodes = Set('Barcode')
 
 
 class GoodsProperty(db.Entity):
@@ -89,6 +92,7 @@ class GoodsProperty(db.Entity):
     id_owner = Required(Goods)
     document_tables = Set(DocumentGoods)
     # adr_document_tables = Set('RS_adr_docs_table')
+    barcodes = Set('Barcode')
 
 
 class Series(db.Entity):
@@ -103,6 +107,7 @@ class Series(db.Entity):
     document_tables = Set(DocumentGoods)
     # adr_document_table = Set('RS_adr_docs_table')
     type_goods = Required('GoodsType')
+    barcodes = Set('Barcode')
 
 
 class GoodsType(db.Entity):
@@ -118,29 +123,61 @@ class GoodsUnit(db.Entity):
     _table_ = 'RS_units'
 
     id = PrimaryKey(str, auto=True)
-    code = Required(str)
+    code = Optional(str)
     name = Required(str)
     nominator = Required(int)
     denominator = Required(int)
-    int_reruction = Required(str)
+    int_reduction = Optional(str)
 
     id_owner = Required(Goods)
     document_tables = Set(DocumentGoods)
-    adr_docs_table = Set('RS_adr_docs_table')
-#
-#
-# class RS_price_types(db.Entity):
-#     id = PrimaryKey(int, auto=True)
-#     document_tables = Set('RS_docs_table')
-#     name = Optional(str)
-#
-#
-# class RS_cells(db.Entity):
-#     id = PrimaryKey(int, auto=True)
-#     name = Optional(str)
-#     barcode = Optional(str)
-#     document_tables = Set('RS_docs_table')
-#     adr_docs_table = Set('RS_adr_docs_table')
+    # adr_docs_table = Set('RS_adr_docs_table')
+    barcodes = Set('Barcode')
+
+
+class PriceType(db.Entity):
+    _table_ = 'RS_price_types'
+
+    id = PrimaryKey(str, auto=True)
+    name = Required(str)
+
+    document_tables = Set(DocumentGoods)
+
+
+class Cell(db.Entity):
+    _table_ = 'RS_cells'
+
+    id = PrimaryKey(str, auto=True)
+    name = Required(str)
+    barcode = Optional(str)
+
+    document_tables = Set(DocumentGoods)
+    # adr_docs_table = Set('RS_adr_docs_table')
+
+
+class BarcFlow(db.Entity):
+    _table_ = 'RS_barc_flow'
+
+    id_doc = Required(Document)
+    barcode = Optional(str)
+
+
+class Barcode(db.Entity):
+    _table_ = 'RS_barcodes'
+
+    barcode = PrimaryKey(str)
+
+    id_good = Required(Goods)
+    id_property = Optional(GoodsProperty)
+    id_series = Optional(Series)
+    id_unit = Optional(GoodsUnit)
+
+
+class ErrorLog(db.Entity):
+    _table_ = 'Error_log'
+    log = Optional(str, default="")
+    timestamp = Required(datetime, sql_default='CURRENT_TIMESTAMP')
+
 #
 #
 # class RS_adr_docs(db.Entity):
@@ -173,17 +210,6 @@ class GoodsUnit(db.Entity):
 #     table_type = Required(str, sql_default='out')
 #
 #
-# class RS_barc_flow(db.Entity):
-#     id_doc = Required('RS_docs')
-#     barcode = Optional(str)
-#
-#
-# class RS_barcodes(db.Entity):
-#     barcode = PrimaryKey(str)
-#     id_goods = Required(str)
-#     id_property = Optional(str)
-#     id_series = Optional(str)
-#     id_unit = Optional(str)
 #
 #
 # class RS_classifier_units(db.Entity):
@@ -216,15 +242,18 @@ models = [
     GoodsType,
     Series,
     GoodsUnit,
-    # RS_price_types,
-    # RS_cells,
+    PriceType,
+    Cell,
+    Barcode,
+    BarcFlow,
+    ErrorLog
+    # RS_constants,
     # RS_adr_docs,
     # RS_adr_docs_table,
-    # RS_barc_flow,
-    # RS_barcodes,
+    # RS_doc_types
     # RS_classifier_units,
-    # RS_constants
-
+    # RS_marking_codes
+    # RS_prices
 ]
 
 db.generate_mapping(create_tables=True)
