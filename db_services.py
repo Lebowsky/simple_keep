fd21df0af496dbfb7284029c3602ce2dfe4b31bd
Raiden71 <wasa@mail.ru>
2023-06-28 08:37:35 +0300

    Промежуточный результат

--- db_services.py
+++ db_services.py
@@ -296,7 +296,7 @@ class DocService:
         return res
 
     def get_doc_details_data(self, id_doc) -> list:
-        query = """
+        query = f"""
             SELECT
             RS_docs_table.id,
             RS_docs_table.id_doc,
@@ -312,23 +312,24 @@ class DocService:
             RS_units.name as units_name,
             RS_docs_table.qtty,
             RS_docs_table.qtty_plan,
-            RS_docs_table.price,
             RS_price_types.name as price_name,
-            RS_docs_table.qtty_plan - RS_docs_table.qtty as IsDone
-            FROM RS_docs_table 
+            RS_docs_table.qtty_plan -RS_docs_table.qtty as IsDone
+            
+            FROMRS_docs_table 
 
             LEFT JOIN RS_goods 
             ON RS_goods.id=RS_docs_table.id_good
             LEFT JOIN RS_properties
-            ON RS_properties.id = RS_docs_table.id_properties
+            ON RS_properties.id =RS_docs_table.id_properties
             LEFT JOIN RS_series
-            ON RS_series.id = RS_docs_table.id_series
+            ON RS_series.id =RS_docs_table.id_series
             LEFT JOIN RS_units
-            ON RS_units.id=RS_docs_table.id_unit
+            ON RS_units.id = RS_docs_table.id_unit
             LEFT JOIN RS_price_types
-            ON RS_price_types.id = RS_docs_table.id_price
+            ON RS_price_types.id =RS_docs_table.id_price
+
             WHERE id_doc = $arg1
-            ORDER BY RS_docs_table.last_updated DESC 
+            ORDER BYRS_docs_table.last_updated DESC 
             """
         res = self._get_query_result(query, (id_doc,), return_dict=True)
         return res
@@ -405,312 +406,60 @@ class DocService:
             get_query_result(qtext, (Err_value,))
 
 class AdrDocService(DocService):
+    def __init__(self):
+        self.docs_table_name = 'RS_Adr_docs'
+        self.details_table_name = 'RS_adr_docs_table'
+        self.isAdr = True
 
-    def get_last_edited_goods(self, to_json=False):
-        query_docs = f'SELECT * FROM RS_adr_docs WHERE id_doc = ? and verified = 1'
 
-        query_goods = '''
-        SELECT * FROM RS_adr_docs_table
-        WHERE id_doc = ? and (sent = 0 or sent is Null)
-        '''
+    def get_current_cell(self):
 
-        try:
-            res_docs = get_query_result(query_docs, (self.doc_id,), True)
-            res_goods = get_query_result(query_goods, (self.doc_id,), True)
-        except Exception as e:
-            raise e
-            # return {'Error': e.args[0]}
 
-        if not res_goods:
-            return None
-
-        for item in res_docs:
-            filtered_list = [d for d in res_goods if d['id_doc'] == item['id_doc']]
-            item['RS_adr_docs_table'] = filtered_list
-
-        if to_json:
-            return json.dumps(res_docs)
-        else:
-            return res_docs
-
-    def update_data_from_json(self, json_data):
-        try:
-            data = json.loads(json_data)
-        except:
-            data = json_data
-
-        if data.get('RS_adr_docs'):
-            self.update_docs(data)
-        else:
-            self.update_nsi(data)
-
-    def update_docs(self, data):
-        doc_ids = ','.join([f'"{item.get("id_doc")}"' for item in data['RS_docs']])
-
-        query = f'''
-            SELECT id_doc, verified
-            FROM RS_adr_docs
-            WHERE id_doc IN ({doc_ids})
-        '''
-        docs = {item['id_doc']: item['verified'] for item in self._get_query_result(query_text=query, return_dict=True)}
-
-        queries = self.json_to_sqlite_query(data, docs)
-
-        for query in queries:
-            try:
-                self._get_query_result(query)
-            except Exception as e:
-                raise e
-
-        return docs
-
-    def update_nsi(self, data):
-        queries = self.json_to_sqlite_query(data)
-
-        for query in queries:
-            try:
-                self._get_query_result(query)
-            except Exception as e:
-                raise e
-
-    def update_sent_data(self, data):
-        if data:
-            for doc in data:
-                id_doc = doc['id_doc']
-                id_goods = ','.join([f'"{item.get("id_good")}"' for item in doc.get('RS_adr_docs_table', [])])
-
-                query = f'''
-                    UPDATE RS_adr_docs_table
-                    SET sent = 1
-                    WHERE id_doc = '{id_doc}' AND id_good IN ({id_goods})
-                    '''
-
-                self._get_query_result(query)
-
-    def json_to_sqlite_query(self, data, docs=None):
-        qlist = []
-        # Цикл по именам таблиц
-        table_list = (
-            'RS_doc_types', 'RS_goods', 'RS_properties', 'RS_units', 'RS_types_goods', 'RS_series', 'RS_countragents',
-            'RS_warehouses', 'RS_price_types', 'RS_cells', 'RS_barcodes', 'RS_prices', 'RS_doc_types', 'RS_docs',
-            'RS_docs_table', 'RS_docs_barcodes', 'RS_adr_docs', 'RS_adr_docs_table')  # ,, 'RS_barc_flow'
-        table_for_delete = ('RS_docs_table', 'RS_docs_barcodes, RS_adr_docs_table')  # , 'RS_barc_flow'
-        doc_id_list = []
-        for table_name in table_list:
-            if not data.get(table_name):
-                continue
-
-            # Добавим в запросы удаление из базы строк тех документов, что мы загружаем
-            if table_name in table_for_delete:
-                query = f"DELETE FROM {table_name} WHERE id_doc in ({', '.join(doc_id_list)}) "
-                qlist.append(query)
-
-            column_names = data[table_name][0].keys()
-            if 'mark_code' in column_names:
-                query_col_names = list(column_names)
-                query_col_names.append('GTIN')
-                query_col_names.append('Series')
-                query_col_names.remove('mark_code')
-            else:
-                query_col_names = list(column_names)
-
-            if docs and table_name == 'RS_docs':
-                query_col_names.append('verified')
-
-            query = f"REPLACE INTO {table_name} ({', '.join(query_col_names)}) VALUES "
-            values = []
-
-            for row in data[table_name]:
-                row_values = []
-                list_quoted_fields = ('name', 'full_name', "mark_code")
-                for col in column_names:
-                    if col in list_quoted_fields and "\"" in row[col]:
-                        row[col] = row[col].replace("\"", "\"\"")
-                    if row[col] is None:
-                        row[col] = ''
-                    if col == 'mark_code':  # Заменяем это поле на поля GTIN и Series
-                        barc_struct = self.parse_barcode(row[col])
-                        row_values.append(barc_struct['GTIN'])
-                        row_values.append(barc_struct['Series'])
-                    else:
-                        row_values.append(row[col])  # (f'"{row[col]}"')
-                    if col == 'id_doc' and table_name == 'RS_docs':
-                        doc_id_list.append('"' + row[col] + '"')
-
-                if docs and table_name == 'RS_docs':
-                    row_values.append(docs[row['id_doc']])
-
-                formatted_val = [f'"{x}"' if isinstance(x, str) else str(x) for x in row_values]
-                values.append(f"({', '.join(formatted_val)})")
-            query += ", ".join(values)
-            qlist.append(query)
-
-        return qlist
-
-    @staticmethod
-    def _get_query_result(query_text, args=None, return_dict=False):
-        return get_query_result(query_text, args=args, return_dict=return_dict)
-
-    def set_doc_value(self, key, value):
-        query = f'''
-            UPDATE RS_adr_docs
-            SET {key} = {value}
-            WHERE id_doc = "{self.doc_id}"
-            '''
-
-        self._get_query_result(query)
-
-    def get_doc_value(self, key, id_doc):
-        query = f'SELECT {key} from RS_adr_docs  WHERE id_doc = ?'
-        res = self._get_query_result(query, (id_doc,), True)
-        if res:
-            return res[0][key]
-
-    def get_doc_types(self) -> list:
-        query = 'SELECT DISTINCT doc_type from RS_adr_docs'
-        doc_types = [rec[0] for rec in self._get_query_result(query)]
-        return doc_types
-
-    # def get_doc_view_data(self, doc_type='', doc_status='') -> list:
-    #     query_text = """
-    #     SELECT RS_adr_docs.id_doc,
-    #     RS_adr_docs.doc_type,
-    #     RS_adr_docs.doc_n,
-    #     RS_adr_docs.doc_date,
-    #     RS_adr_docs.id_warehouse,
-    #     ifnull(RS_warehouses.name,'') as RS_warehouse,
-    #     RS_adr_docs.verified,
-    #     RS_adr_docs.sent,
-    #     RS_adr_docs.add_mark_selection
-    #
-    #      FROM RS_adr_docs
-    #
-    #      LEFT JOIN RS_warehouses as RS_warehouses
-    #      ON RS_warehouses.id=RS_adr_docs.id_warehouse
-    #      """
-    #     where = []
-    #     if not (doc_type == '' or doc_type == 'Все'):
-    #         where.append("RS_adr_docs.doc_type=?")
-    #
-    #     if doc_status:
-    #         if doc_status == "Выгружен":
-    #             where.append("sent=1 AND verified=1")
-    #         elif doc_status == "К выгрузке":
-    #             where.append("ifnull(verified,0)=1 AND ifnull(sent,0)=0")
-    #         elif doc_status == "К выполнению":
-    #             where.append("ifnull(verified,0)=0 AND ifnull(sent,0)=0")
-    #         elif doc_status == "Все":
-    #             where = ""
-    #
-    #     where_text = ' WHERE ' + ' AND '.join(where)
-    #
-    #     if not doc_type or doc_type == "Все":
-    #         args_tuple = None
-    #
-    #     else:
-    #         args_tuple = (doc_type,)
-    #
-    #     query_text = f'''
-    #          {query_text}
-    #          {where_text}
-    #          ORDER BY RS_adr_docs.doc_date
-    #      '''
-    #
-    #     result = self._get_query_result(query_text, args_tuple, return_dict=True)
-    #     return result
-
-    def delete_doc(self, id_doc):
-        query_doc = 'DELETE FROM RS_docs WHERE id_doc = ?'
-        res = self._get_query_result(query_doc, (id_doc,))
-
-        return res
-
-    def get_docs_stat(self):
-        query = '''
-        WITH tmp AS (
-            SELECT 
-                doc_type,
-                RS_docs.id_doc,
-                1 as doc_Count,
-                IFNULL(RS_docs.sent,0) as sent,
-                IFNULL(verified,0) as verified, 
-                CASE WHEN IFNULL(verified,0)=0 THEN 
-                    COUNT(RS_docs_table.id)
-                ELSE 
-                    0 
-                END as count_verified,
-                CASE WHEN IFNULL(verified,0)=1 THEN 
-                    count(RS_docs_table.id)
-                ELSE 
-                    0 
-                END as count_unverified,
-                CASE WHEN IFNULL(verified,0)=0 THEN
-                     SUM(RS_docs_table.qtty_plan)
-                ELSE 
-                    0 
-                END as qtty_plan_verified,
-                CASE WHEN IFNULL(verified,0)=1 THEN 
-                    SUM(RS_docs_table.qtty_plan)
-                ELSE 
-                    0 
-                END as qtty_plan_unverified
-            FROM RS_docs
-            LEFT JOIN RS_docs_table 
-                ON RS_docs_table.id_doc = RS_docs.id_doc
-            GROUP BY RS_docs.id_doc
-        )
-        SELECT 
-            doc_type as docType, 
-            COUNT(id_doc),
-            SUM(doc_Count) as count, 
-            SUM(sent) as sent, 
-            SUM(verified) as verified,
-            SUM(count_verified) as count_verified,
-            SUM(count_unverified) as count_unverified,
-            SUM(qtty_plan_verified) as qtty_plan_verified,
-            SUM(qtty_plan_unverified) as qtty_plan_unverified
-        FROM tmp
-        GROUP BY doc_type
-        '''
-
-        res = self._get_query_result(query, return_dict=True)
-        return res
 
     def get_doc_details_data(self, id_doc) -> list:
-        query = """
-            SELECT
-            RS_docs_table.id,
-            RS_docs_table.id_doc,
-            RS_docs_table.id_good,
-            RS_goods.name as good_name,
-            RS_goods.code,
-            RS_goods.art,
-            RS_docs_table.id_properties,
-            RS_properties.name as properties_name,
-            RS_docs_table.id_series,
-            RS_series.name as series_name,
-            RS_docs_table.id_unit,
-            RS_units.name as units_name,
-            RS_docs_table.qtty,
-            RS_docs_table.qtty_plan,
-            RS_docs_table.price,
-            RS_price_types.name as price_name,
-            RS_docs_table.qtty_plan - RS_docs_table.qtty as IsDone
-            FROM RS_docs_table 
+        query_text = '''SELECT
+            RS_adr_docs_table.id,
+            RS_adr_docs_table.id_doc,
+            RS_adr_docs_table.id_good,
+            ifnull(RS_goods.name, :NullValue) as good_name,
+            ifnull(RS_goods.code, :EmptyString) as code,
+            ifnull(RS_goods.art, :EmptyString) as art,
+            ifnull(RS_adr_docs_table.id_properties, :EmptyString) as id_properties,
+            ifnull(RS_properties.name, :EmptyString) as properties_name,
+            ifnull(RS_adr_docs_table.id_series, :EmptyString) as id_series,
+            ifnull(RS_series.name, :EmptyString) as series_name,
+            ifnull(RS_adr_docs_table.id_unit, :EmptyString) as id_unit,
+            ifnull(RS_units.name, :EmptyString) as units_name,
+            RS_adr_docs_table.qtty as qtty,
+            RS_adr_docs_table.qtty_plan as qtty_plan,
+            RS_adr_docs_table.qtty_plan - RS_adr_docs_table.qtty as IsDone,
+            ifnull(RS_adr_docs_table.id_cell, :EmptyString) as id_cell,
+            ifnull(RS_cells.name, :NullValue) as cell_name
+            
+            
+            FROM RS_adr_docs_table 
 
             LEFT JOIN RS_goods 
-            ON RS_goods.id=RS_docs_table.id_good
+            ON RS_goods.id=RS_adr_docs_table.id_good
             LEFT JOIN RS_properties
-            ON RS_properties.id = RS_docs_table.id_properties
+            ON RS_properties.id = RS_adr_docs_table.id_properties
             LEFT JOIN RS_series
-            ON RS_series.id = RS_docs_table.id_series
+            ON RS_series.id = RS_adr_docs_table.id_series
             LEFT JOIN RS_units
-            ON RS_units.id=RS_docs_table.id_unit
-            LEFT JOIN RS_price_types
-            ON RS_price_types.id = RS_docs_table.id_price
-            WHERE id_doc = $arg1
-            ORDER BY RS_docs_table.last_updated DESC 
-            """
+            ON RS_units.id=RS_adr_docs_table.id_unit
+            LEFT JOIN RS_cells
+            ON RS_cells.id=RS_adr_docs_table.id_cell
+
+            WHERE id_doc = :id_doc and table_type = :table_type
+            
+            '''
+
+        if self.curCell:
+            query_text = query_text + '''
+             and (id_cell=:current_cell OR id_cell="" OR id_cell is Null)
+            '''
+
+        query_text = query_text + ' ORDER BY RS_cells.name, RS_adr_docs_table.last_updated DESC'
         res = self._get_query_result(query, (id_doc,), return_dict=True)
         return res
 
