f55e6520cc33166a614b64ad6898b1f47d738411
And <ase@crimea.com>
2023-07-11 20:03:26 +0300

    add get ip address method

--- ui_utils.py
+++ ui_utils.py
@@ -246,9 +246,13 @@ class RsDoc(Rs_doc):
 
 
 def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
-        return s.getsockname()[0]
+        ip_address = s.getsockname()[0]
     except Exception as e:
-        return None
+        ip_address = None
+    finally:
+        s.close()
+
+    return ip_address
