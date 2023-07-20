9be221f785ff2f2cbbab6e6a008c6094be4a1d05
And <ase@crimea.com>
2023-07-20 14:07:51 +0300

    remove duplicate method

--- ui_utils.py
+++ ui_utils.py
@@ -33,9 +33,6 @@ class HashMap:
         self.hash_map.put('FinishProcess', '')
 
 
-    def finish_process(self):
-        self.hash_map.put('FinishProcess','')
-
     def toast(self, text, add_to_log=False):
         self.hash_map.put('toast', str(text))
         if add_to_log:
