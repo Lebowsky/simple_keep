a52dcae534fd99b07e05f0f9e6b55a3171b4e58e
Gekk0 <gekk0dw@gmail.com>
2023-10-20 18:10:37 +0300

    #104003 modal fix

--- ui_models.py
+++ ui_models.py
@@ -1724,13 +1724,34 @@ class GroupScanDocsListScreen(DocsListScreen):
                    f"в {current_mode.upper()} режиме.")
             if self.hash_map.get_json('selected_card_data')['is_group_scan'] == '0':
                 msg += warning_msg
-            self.hash_map.put('group_scan_message', msg)
+            self.hash_map.put('msg', msg)
 
-            self.hash_map.show_dialog('Подтвердите действие')
+            layout = '''{
+                                    "type": "LinearLayout",
+                                    "Variable": "",
+                                    "orientation": "horizontal",
+                                    "height": "wrap_content",
+                                    "width": "match_parent",
+                                    "weight": "0",
+                                    "Elements": [
+                                        {
+                                            "Value": "@msg",
+                                            "Variable": "msg",
+                                            "height": "wrap_content",
+                                            "width": "match_parent",
+                                            "weight": "0",
+                                            "type": "TextView"
+                                        }
+                                    ]
+                                }'''
+
+            self.hash_map.show_dialog('open_doc_confirmation',
+                                      title="Подтвердите действие", dialog_layout=layout)
+
             selected_card_key = self.hash_map['selected_card_key']
             self.hash_map['id_doc'] = selected_card_key
 
-        elif self._is_result_positive('Подтвердите действие'):
+        elif self._is_result_positive('open_doc_confirmation'):
             id_doc = self.hash_map['id_doc']
             self.service.doc_id = id_doc
             if self.hash_map.get_json('selected_card_data')['is_group_scan'] == '0':
@@ -1757,6 +1778,9 @@ class GroupScanDocsListScreen(DocsListScreen):
             else:
                 self.toast('При очистке данных пересчета возникла ошибка.')
                 self.hash_map.error_log(res.get('error'))
+
+    def can_launch_timer(self):
+        return False
 
     def _back_screen(self):
         MainEvents.start_timer(self.hash_map)
@@ -2288,7 +2312,7 @@ class DocDetailsScreen(Screen):
         screen.show()
 
     def _show_dialog_input_barcode(self):
-        loyaut = '''{
+        layout = '''{
             "type": "LinearLayout",
             "Variable": "",
             "orientation": "horizontal",
@@ -2306,7 +2330,8 @@ class DocDetailsScreen(Screen):
                 }
             ]
         }'''
-        self.hash_map.show_dialog('modal_dialog_input_barcode', title="Введите штрихкод товара", dialog_layout=loyaut)
+        self.hash_map.show_dialog('modal_dialog_input_barcode',
+                                  title="Введите штрихкод товара", dialog_layout=layout)
 
     class TextView(widgets.TextView):
         def __init__(self, value):
